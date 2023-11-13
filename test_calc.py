6ca53786ad1bc5a48da83d83389649e341f4f1c2
CodeCraftersAcademy <150399595+CodeCraftersAcademy@users.noreply.github.com>
2023-11-13 20:05:34 -0300

    Update test_calc.py
    nao passando testes

--- test_calc.py
+++ test_calc.py
@@ -5,5 +5,5 @@ def test_15_add_15_equal_30():
     assert funcs.calc(15, 15, 'add') == 30
 
 def test_15_add_15_equal_28():
-    assert funcs.calc(15, 13, 'add') == 28
+    assert funcs.calc(15, 13, 'add') == 30
 
